3e6beef957ea8b6f4664bab899b3f0c86099f96c
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-02-08 12:21:10 +0200

    data.json

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
-import { data } from './src/data'
-import { text } from 'd3'
+import * as data from './src/data.json'
 
 document.addEventListener('DOMContentLoaded', () => {
     const svg = document.querySelector('svg')
     const svge = (tag: string) =>
         document.createElementNS('http://www.w3.org/2000/svg', tag)
 
-    data.forEach(d => {
+    console.log('typeof', typeof data)
+    Object.values(data).forEach(d => {
         console.log(d)
         const circle = svge('circle')
         const text = svge('text')
-        text.textContent = d.label
+        text.textContent = d.data
 
         circle.addEventListener('mouseover', e => {
             console.log(d.label)
         })
 
-        circle.setAttribute('cx', `${d.x}`)
-        circle.setAttribute('cy', `${d.y}`)
-        circle.setAttribute('cluster', `${d.cluster}`)
+        circle.setAttribute('cx', `${d.x * 30}`)
+        circle.setAttribute('cy', `${d.y * 30}`)
+        circle.setAttribute('label', `${d.label}`)
 
-        text.setAttribute('x', `${d.x + 10}`)
-        text.setAttribute('y', `${d.y}`)
+        text.setAttribute('x', `${d.x * 30 + 10}`)
+        text.setAttribute('y', `${d.y * 30}`)
 
         svg?.appendChild(circle)
         svg?.appendChild(text)
