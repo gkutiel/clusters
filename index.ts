8b0cb220cd4278157537c4c1cfe112575860c181
Gilad Kutiel <gkutiel@amazon.com>
2020-02-09 08:36:28 +0200

    clusters

--- index.ts
+++ index.ts
@@ -9,16 +9,31 @@ type point = {
 const data: point[] = require('./src/data.json')
 
 document.addEventListener('DOMContentLoaded', () => {
+    const xs = data.map(d => d.x)
+    const ys = data.map(d => d.y)
+    const max_x = Math.max(...xs)
+    const min_x = Math.min(...xs)
+    const max_y = Math.max(...ys)
+    const min_y = Math.min(...ys)
+
+    const scale_x = (x: number) =>
+        (x - min_x) * 100 / (max_x - min_x)
+
+    const scale_y = (y: number) =>
+        (y - min_y) * 100 / (max_y - min_y)
+
+    console.log(min_x, max_x, min_y, max_y)
+
     const svg = document.querySelector('svg')
     const txt = document.querySelector('#text')
 
     const svge = (tag: string) =>
         document.createElementNS('http://www.w3.org/2000/svg', tag)
 
-    const p = (txt: string) => {
-        const p = document.createElement('p')
-        p.innerText = txt
-        return p
+    const t = (txt: string) => {
+        const i = document.createElement('li')
+        i.innerText = txt
+        return i
     }
 
     data.forEach(d => {
@@ -28,14 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(d.label)
         })
 
-        circle.setAttribute('cx', `${d.x * 30}`)
-        circle.setAttribute('cy', `${d.y * 30}`)
+        circle.setAttribute('cx', `${scale_x(d.x)}`)
+        circle.setAttribute('cy', `${scale_y(d.y)}`)
         circle.setAttribute('label', `${d.label}`)
 
         // text.setAttribute('x', `${d.x * 30 + 10}`)
         // text.setAttribute('y', `${d.y * 30}`)
 
         svg?.appendChild(circle)
-        txt?.appendChild(p(d.data))
+        txt?.appendChild(t(d.data))
     })
 })
